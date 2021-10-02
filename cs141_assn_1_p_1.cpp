8ebe3b641dbe84a0586d1f70bcda03edc1e80605
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2021-10-02 12:48:44 -0700

    merge_func_wrong but wanted to upload in case breaks happen

--- cs141_assn_1_p_1.cpp
+++ cs141_assn_1_p_1.cpp
@@ -1,23 +1,113 @@
-//#include <iostream> 
+//#include <iostream>
 //using namespace std;
 //#include <vector>
-#include "stdio.h" 
+#include "stdio.h"
 #include <vector>
 
-int main(){
-	//gonna use for starting length of array 
-    //unsigned int n;
-	int n;//n is number of inputs
-	//std::cin >> n; 
-    scanf("%i", &n);
-   
-    //std::vector<int> array(n);
-    std::vector<int> array(n); 
-    unsigned int input;
+//homebrew merge_sort with psudeo-code help
+std::vector<int> merge(std::vector<int> , std::vector<int> );
+int test_func();
+
+std::vector<int> merge_sort(std::vector<int> array_input, int n_input)
+{
+    // if (n_input <= 1)
+    // {
+    //     printf("N_INPUT <=1\n");
+    //     //return array_input
+    //     //return array_input;
+    // }
+
     
-    for (int i = 0; i < n; i++){
-        //std::cin >> input; 
-        
+    //do low + ((high-low)/2)
+    
+    if (n_input == 1){
+        return array_input;
+    }
+    //if (n_input >1){
+    
+    std::vector<int> left_array(array_input.begin(),
+                                array_input.begin() + (n_input/2));
+    printf("LEFT ARRAY\n");
+    for (int i = 0; i < left_array.size(); i++){
+        ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+        printf("array at %i: %i \n", i, left_array[i]);
+    }
+
+    
+    std::vector<int> right_array(array_input.begin() + ((n_input/2)+1), array_input.end());
+
+    printf("RIGHT ARRAY\n");
+    for (int i = 0; i < right_array.size(); i++){
+        ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+        printf("array at %i: %i \n", i, right_array[i]);
+    }
+    
+    merge_sort(left_array, (n_input / 2));
+    merge_sort(right_array, (n_input / 2));
+    
+    std::vector<int> merged_array;
+    
+    return merge(left_array, right_array);
+ 
+    
+}
+
+std::vector<int> merge(std::vector<int> left_input, std::vector<int> right_input)
+{
+    int left_input_index = 0;
+    int right_input_index = 0;
+    std::vector<int> merged_array;
+    int swap;
+    int left_input_end = left_input.size() - 1;
+    int right_input_end = right_input.size() - 1;
+    // int left_input_active=0;
+    // int right_input_active=0;
+    while (left_input_index > left_input_end && right_input_index > right_input_end)
+    {
+        if (left_input[left_input_index] > right_input[right_input_index])
+        {
+            printf("SWAPPING IN MERGE\n");
+            swap = left_input[left_input_index];
+            left_input[left_input_index] = right_input[right_input_index];
+            right_input[right_input_index] = swap;
+
+        } //enf of if statement swap for merge
+        if (left_input_index > left_input_end)
+        {
+            merged_array.push_back(left_input[left_input_index]);
+            left_input_index += 1;
+            
+        }
+        if (right_input_index > right_input_end)
+        {
+            merged_array.push_back(right_input[right_input_index]);
+            right_input_index+=1;
+        }
+    }
+    return merged_array;
+}
+
+int test_func()
+{
+    printf("I LIKE APPLES \n");
+    return -1;
+}
+int main()
+{
+    //gonna use for starting length of array
+    //unsigned int n;
+    int n; //n is number of inputs
+           //std::cin >> n;
+    scanf("%i", &n);
+
+    //std::vector<int> array(n);
+    std::vector<int> array(n);
+    unsigned int input;
+
+    for (int i = 0; i < n; i++)
+    {
+        //std::cin >> input;
+
         scanf("%u", &input);
         array[i] = input;
     }
@@ -28,100 +118,55 @@ int main(){
     //     printf("array at %i: %i \n", i, array[i]);
     // }
 
-    //need to change selection sort from min to max
-    //selection sort
-    int sorted_min = 0;
-    int swap = 0;
-    int sorted_min_index = 0;
-    //int curr_item = 0;
-    //y is curr item
-    int unsorted_min = -4000;
-    int unsorted_min_index =0;
-    //int y;
-    for (int x = 0; x < n; x++){
-        sorted_min = array[x];
-        sorted_min_index = x;
-        //curr_item = x+1;
-        //y = x
-        unsorted_min = -4000;//if its' this give it value
-        //of first array[y] value
-        for (int y = x; y < n; y++){
-            if(unsorted_min == -4000){
-                unsorted_min = array[y];
-                unsorted_min_index = y;
-            }
-            if (array[y] > unsorted_min){
-                unsorted_min = array[y];
-                unsorted_min_index = y;
-            }
-        //   if (array[curr_min] > array[y]){
-        //     swap = array[curr_min];
-        //     array[curr_min] = array[y];
-        //     array[y] = swap;
-        //   }  
-        } // end of inner for loop 
-        if (unsorted_min > sorted_min){
-            swap = array[sorted_min_index];
-            array[sorted_min_index] = array[unsorted_min_index];
-            array[unsorted_min_index] = swap;
+    //MERGE SORT
+    array = merge_sort(array,n);
 
-            // //swap values 
-            // swap = sorted_min;
-            // sorted_min = unsorted_min;
-            // unsorted_min = swap;
+    //just to test what array is set up properly
+    printf("SORTED ARRAY HIGHEST FIRST \n");
+    for (int i = 0; i < n; i++){
+        ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+        printf("array at %i: %i \n", i, array[i]);
+    }
 
-            // //swap indexes
-            // swap = sorted_min_index; 
-            // swap 
-
-        }
-    } // end of outer for loop
-
-    // //just to test what array is set up properly
-    // printf("SORTED ARRAY HIGHEST FIRST \n");
-    // for (int i = 0; i < n; i++){
-    //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
-    //     printf("array at %i: %i \n", i, array[i]);
-    // }
-    //;
     int num_outputs;
-    
-    scanf("%i",&num_outputs);
-    
+
+    scanf("%i", &num_outputs);
+
     //printf("num_output: %i \n", num_outputs);
     std::vector<int> placing(n);
     //int curr_placing;
     int placing_input;
-    
+
     // printf("SORTED ARRAY before getting placing value \n");
     // for (int i = 0; i < n; i++){
     //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
     //     //printf("array at %i: %i \n", i, array[i]);
     // }
 
-    for (int m = 0; m < num_outputs; m++){
-        scanf("%i",&placing_input ); // scan for input again and put it into placing array
+    for (int m = 0; m < num_outputs; m++)
+    {
+        scanf("%i", &placing_input); // scan for input again and put it into placing array
         placing[m] = placing_input;
         //printf("placing: %i ", placing[m]);
-        placing[m] -= 1;//value is just -1 of what placing is 
+        placing[m] -= 1; //value is just -1 of what placing is
         //ex. placing 5 is array[4] placing 1 is array[0] etc
         //printf("placing[m]: %i,\n",placing[m]);
         //printf("placing_value_before %i: array_placing: %i \n", placing[m], array[placing[m]]); // do printifn in other loop because it first gets input and the outputs
-    
     }
-    
+
     // printf("SORTED ARRAY after getting placing value \n");
     // for (int i = 0; i < n; i++){
     //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
     //     printf("array at %i: %i \n", i, array[i]);
     // }
     //printf("placing_value,0 %i: array[0]%i \n", placing[0], array[placing[0]]);
-    
+
     //outputs placing
-    for (int m = 0; m < num_outputs; m++){
+    for (int m = 0; m < num_outputs; m++)
+    {
         //printf("placing_value %i: array_placing: %i \n", placing[m], array[placing[m]]);
         //printf("%i \n", array[placing[m]]);
-        printf("%i\n",array[placing[m]]);
+        printf("%i\n", array[placing[m]]);
     }
 
     // //
@@ -131,8 +176,5 @@ int main(){
     //     printf("array at %i: %i \n", i, array[i]);
     // }
 
-    
-    
-	return 0;
+    return 0;
 }
-
