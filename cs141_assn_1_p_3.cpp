cd26dc77c7f995e0314256545074c56dc73cb00b
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2021-10-04 08:10:04 -0700

    bonus 1 for program assn 1

---
+++ cs141_assn_1_p_3.cpp
@@ -0,0 +1,169 @@
+//#include <iostream> 
+//using namespace std;
+//#include <vector>
+#include "stdio.h" 
+#include <vector>
+ 
+int main(){
+	//gonna use for starting length of array 
+    //unsigned int n;
+	int n;//n is number of inputs
+	//std::cin >> n; 
+    scanf("%i", &n);
+   
+    //std::vector<int> array(n);
+    std::vector<int> array(n); 
+    unsigned int input;
+    
+    for (int i = 0; i < n; i++){
+        //std::cin >> input; 
+        
+        scanf("%u", &input);
+        array[i] = input;
+    }
+ 
+    // //just to test what array is set up properly
+    // for (int i = 0; i < n; i++){
+    //     //std::cout << "array at: " << i << " " << array[i] << std::endl;
+    //     printf("array at %i: %i \n", i, array[i]);
+    // }
+ 
+    //need to change selection sort from min to max
+    //selection sort
+    int sorted_min = 0;
+    int swap = 0;
+    int sorted_min_index = 0;
+    //int curr_item = 0;
+    //y is curr item
+    int unsorted_min = -4000;
+    int unsorted_min_index =0;
+    //int y;
+    for (int x = 0; x < n; x++){
+        sorted_min = array[x];
+        sorted_min_index = x;
+        //curr_item = x+1;
+        //y = x
+        unsorted_min = -4000;//if its' this give it value
+        //of first array[y] value
+        for (int y = x; y < n; y++){
+            if(unsorted_min == -4000){
+                unsorted_min = array[y];
+                unsorted_min_index = y;
+            }
+            if (array[y] > unsorted_min){
+                unsorted_min = array[y];
+                unsorted_min_index = y;
+            }
+        //   if (array[curr_min] > array[y]){
+        //     swap = array[curr_min];
+        //     array[curr_min] = array[y];
+        //     array[y] = swap;
+        //   }  
+        } // end of inner for loop 
+        if (unsorted_min > sorted_min){
+            swap = array[sorted_min_index];
+            array[sorted_min_index] = array[unsorted_min_index];
+            array[unsorted_min_index] = swap;
+ 
+            // //swap values 
+            // swap = sorted_min;
+            // sorted_min = unsorted_min;
+            // unsorted_min = swap;
+ 
+            // //swap indexes
+            // swap = sorted_min_index; 
+            // swap 
+ 
+        }
+    } // end of outer for loop
+ 
+    // //just to test what array is set up properly
+    // printf("SORTED ARRAY HIGHEST FIRST \n");
+    // for (int i = 0; i < n; i++){
+    //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+    //     printf("array at %i: %i \n", i, array[i]);
+    // }
+    //;
+    int num_outputs;
+    
+    scanf("%i",&num_outputs);
+    
+    //printf("num_output: %i \n", num_outputs);
+    std::vector<int> num_to_search(n);
+    //int curr_placing;
+    int num_to_search_input;
+    
+    // printf("SORTED ARRAY before getting placing value \n");
+    // for (int i = 0; i < n; i++){
+    //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+    //     //printf("array at %i: %i \n", i, array[i]);
+    // }
+ 
+    for (int m = 0; m < num_outputs; m++){
+        scanf("%i",&num_to_search_input ); // scan for input again and put it into placing array
+        num_to_search[m] = num_to_search_input;
+        //printf("placing: %i ", placing[m]);
+        num_to_search[m] -= 1;//value is just -1 of what placing is 
+        //ex. placing 5 is array[4] placing 1 is array[0] etc
+        //printf("placing[m]: %i,\n",num_to_search[m]);
+        //printf("placing_value_before %i: array_placing: %i \n", placing[m], array[placing[m]]); // do printifn in other loop because it first gets input and the outputs
+    
+    }
+
+    //printf("DISTINCT ARRAY level\n");
+    int last_value= -98128;
+    std::vector<int> distinct_array;
+    for (int i = 0; i < n; i++){
+        if (last_value==-98128){
+            last_value= array[i];
+            //printf("Setting initial last_value to %i \n", last_value);
+            distinct_array.push_back(last_value);//pushes back last_value initial
+            continue;
+        }
+        if(array[i] == last_value){
+            //does not push_back
+            continue; // continues to next value since not distinct
+        }
+        else{
+            //printf("Pushing back distinct value %i \n", last_value);
+            distinct_array.push_back(array[i]);
+            last_value = array[i];
+        }
+
+
+    }
+
+    //printf("DISTINCT ARRAY  \n");
+    for (int i = 0; i < n; i++){
+        ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+        //printf("DISTINCT ARRAY at %i: %i \n", i, distinct_array[i]);
+    }
+    
+    // printf("SORTED ARRAY after getting placing value \n");
+    // for (int i = 0; i < n; i++){
+    //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+    //     printf("array at %i: %i \n", i, array[i]);
+    // }
+    
+    //printf("placing_value,0 %i: array[0]%i \n", placing[0], array[placing[0]]);
+    
+ 
+    // //
+    // printf("SORTED ARRAY RIGHT BEFORE END \n");
+    // for (int i = 0; i < n; i++){
+    //     ////std::cout << "array at: " << i << " " << array[i] << std::endl;
+    //     printf("array at %i: %i \n", i, array[i]);
+    // }
+ 
+    
+    //outputs distinct placing
+    for (int m = 0; m < num_outputs; m++)
+    {
+        //printf("placing_value %i: array_placing: %i \n", placing[m], array[placing[m]]);
+        //printf("%i \n", array[placing[m]]);
+
+        printf("%i\n", distinct_array[num_to_search[m]]);
+    }
+    
+	return 0;
+}
