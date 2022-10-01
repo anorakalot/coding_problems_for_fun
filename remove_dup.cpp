6da02f955bf40749f7748d7f3cbe4a8ac8dc16c7
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-09-30 19:15:40 -0700

    wip #3 fixed bug with num_non_recur going off of extra zeroes

--- remove_dup.cpp
+++ remove_dup.cpp
@@ -8,19 +8,31 @@ public:
         for (int x =0; x < nums.size(); x++){
             //printf("%i\n",nums[x]);
             if (nums[x] == nums[x+1]){
-                //num_non_recur -=1;
-                nums[x+1] = NULL;
+                if (nums[x] != -101){
+                    num_non_recur -=1;
+                }
+                //it's picking up zeroes at the end that's why num-non_recur keeps
+                //getting lowered
+                printf("(in_btw) num_non_recur = %i\n\n",num_non_recur);
+                //nums[x+1] = NULL;
+                nums[x+1] = -101;
                 for (int y = x+1; y < nums.size(); y++){
                     nums[y] =nums[y+1];
-                    nums[y+1] = NULL;
+                    //nums[y+1] = NULL;
+                    nums[y+1] = -101;
                 }
                 
             }//end of if 
             else if (nums[x] == nums[x-1]){
-                nums[x] = NULL;
+                if (nums[x] != -101){
+                num_non_recur -=1;
+                }
+                //nums[x] = NULL;
+                nums[x] = -101;
                 for (int y = x;y< nums.size(); y++){
                     nums[y] = nums[y+1];
-                    nums[y+1] = NULL;
+                    //nums[y+1] = NULL;
+                    nums[y+1] = -101;
                 }
                 
             }//end of else if
