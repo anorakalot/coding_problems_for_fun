a0c6daf994b4f199207bf696039e9361d82e8cab
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-10-01 19:50:49 -0700

    wip #4 fixed bug [1,1] input

--- remove_dup.cpp
+++ remove_dup.cpp
@@ -5,7 +5,13 @@ public:
         
         int num_non_recur = nums.size();
         printf("num_non_recur = %i\n\n",num_non_recur);
+        if (num_non_recur == 1){//to handle outlier case of 1 
+            return num_non_recur;
+        }
         for (int x =0; x < nums.size(); x++){
+            if (nums[x] == -101){
+                break;
+            }
             //printf("%i\n",nums[x]);
             if (nums[x] == nums[x+1]){
                 if (nums[x] != -101){
@@ -17,7 +23,12 @@ public:
                 //nums[x+1] = NULL;
                 nums[x+1] = -101;
                 for (int y = x+1; y < nums.size(); y++){
-                    nums[y] =nums[y+1];
+                    printf("test_1\n");
+                    if (y == nums.size()-1){
+                        break;
+                    }
+                    nums[y] =nums[y+1];//error is here stops at test 1
+                    printf("test_2\n");
                     //nums[y+1] = NULL;
                     nums[y+1] = -101;
                 }
@@ -42,6 +53,7 @@ public:
             }
             printf("\n\n");
             printf("END OF DEBUG LOOP\n");
+            
         }//end of for loop
         printf("num_non_recur = %i",num_non_recur);
         return num_non_recur;
