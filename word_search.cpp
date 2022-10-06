7d1bab9758e2fc4ded3f22cedf288883859fbb68
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-10-06 12:19:04 -0700

    wip #2 just switched the y and x for 2d array trav

--- word_search.cpp
+++ word_search.cpp
@@ -4,9 +4,9 @@ public:
         //have it go through each possible neighbor to see if word can be done
         std::cout<<word[0]<< " " << std::endl;
         printf("print out board for debug\n");
-        for (int x = 0; x < board.size(); x++){
-            for (int y = 0; y < board[x].size(); y++){
-                printf("%c ",board[x][y]);
+        for (int y = 0; y < board.size(); y++){
+            for (int x = 0; x < board[x].size(); x++){
+                printf("%c ",board[y][x]);
             }
             printf("\n");
         }
@@ -19,9 +19,9 @@ public:
         int found_first_letter =0;
         int valid_word = 0;
         printf("going through board to \n");
-        for (int x = 0; x < board.size(); x++){
-            for (int y = 0; y < board[x].size(); y++){
-                if (board[x][y] == word[0]){//if it finds the first letter in the word sequence
+        for (int y = 0; y < board.size(); y++){
+            for (int x = 0; x < board[x].size(); x++){
+                if (board[y][x] == word[0]){//if it finds the first letter in the word sequence
                     x_save_space = x;
                     y_save_space = y;
 
@@ -31,38 +31,41 @@ public:
                     valid_word = 1;
                     while (valid_word == 1){//meaning while ongoing word is valid keep going if not go back to 
                                             //x_save_space and y_save_space
-                        if (board[x][y+1] == word[word_index]){
+                        //if (x+1 <board[x].size()){
+                        if (board[y][x+1] == word[word_index]){
                             if(word_index == word.size()-1){//meaning at last part of the word
                                return 1; 
                             }
-                            //x stays the same
-                            y +=1;
+                            //y stays the same
+                            x +=1;
                             
                             word_index +=1;
                             
                         }//end of y+1 
-                        else if (board[x][y-1] == word[word_index]){
+                        //}
+                        
+                        else if (board[y][x-1] == word[word_index]){
                             if(word_index == word.size()-1){//meaning at last part of the word
                                return 1; 
                             }
+                            //y stays the same
+                            x -=1;
+                            word_index +=1;  
+                        }
+                        else if (board[y+1][x] == word[word_index]){
+                            if(word_index == word.size()-1){//meaning at last part of the word
+                               return 1; 
+                            }
+                            y+=1;
                             //x stays the same
+                            word_index +=1;  
+                        }
+                        else if (board[y-1][x] == word[word_index]){
+                            if(word_index == word.size()-1){//meaning at last part of the word
+                               return 1; 
+                            }
                             y -=1;
-                            word_index +=1;  
-                        }
-                        else if (board[x+1][y] == word[word_index]){
-                            if(word_index == word.size()-1){//meaning at last part of the word
-                               return 1; 
-                            }
-                            x = x+1;
-                            //y stays the same
-                            word_index +=1;  
-                        }
-                        else if (board[x-1][y] == word[word_index]){
-                            if(word_index == word.size()-1){//meaning at last part of the word
-                               return 1; 
-                            }
-                            x = x-1;
-                            //y stays the same
+                            //x stays the same
                             word_index +=1;  
                         }
                         else{
@@ -79,9 +82,9 @@ public:
                 }//end of if board[x][y] == word[0] matching letter to first letter in word
                 
                 
-            }//end of for loop y part
+            }//end of for loop x part
             printf("\n");
-        }//end of outer for loop x part
+        }//end of outer for loop y part
         
         
         return 0; //placeholder 
