8a4dc4220ccae4de722013e1ff5bedc705a1dbd4
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-10-05 18:58:35 -0700

    wip #8 fixed issues with missing when to stay at an x
    fixed it by adding if statements at the end that keep it at the same x if there's still stuff equal to it at x+1 and x-1 comparison

--- remove_dup.cpp
+++ remove_dup.cpp
@@ -11,7 +11,7 @@ public:
         //int only_one = 0;
         //int if_odd = 0;
         int redo_once = 0;
-        
+        int redo_cnt = 4;
         for (int x =0; x < nums.size(); x++){
             
             printf("current x = %i\n",x);
@@ -24,9 +24,9 @@ public:
             //printf("%i\n",nums[x]);
             int z = x-1;
             //printf("x-1 = %i\n",z);
-            printf("test_3\n");
+            //printf("test_3\n");
             if (x+1 <=nums.size()-1){//make sure in range of nums[x+1] comparison
-                printf("test_5\n");
+                //printf("test_5\n");
                 if (nums[x] == nums[x+1]){
                     // if (only_one == 0){
                     // only_one = 1;
@@ -41,12 +41,12 @@ public:
                     //nums[x+1] = NULL;
                     nums[x+1] = -101;
                     for (int y = x+1; y < nums.size()-1; y++){
-                        printf("test_1\n");
+                        //printf("test_1\n");
                         if (y == nums.size()-1){
                             break;
                         }
                         nums[y] =nums[y+1];//error is here stops at test 1
-                        printf("test_2\n");
+                        //printf("test_2\n");
                         //nums[y+1] = NULL;
                         nums[y+1] = -101;
                     }
@@ -62,8 +62,8 @@ public:
            //problem is else if not letting it go into this other comparison
             //else if (x-1 >= 0){//make sure there's range for nums[x-1] comparison
              if (x-1 >= 0){//make sure there's range for nums[x-1] comparison
-                printf("test 6\n");
-             
+                //printf("test 6\n");
+  
                 if (nums[x] == nums[x-1]){//bug is here [1,2]
                     // if (only_one == 0){
                     // only_one = 1;
@@ -75,7 +75,7 @@ public:
                     //nums[x] = NULL;
                     nums[x] = -101;
                     for (int y = x;y< nums.size()-1; y++){
-                        printf("inside x-1 comp for loop restructuring vector\n");
+                        //printf("inside x-1 comp for loop restructuring vector\n");
                         nums[y] = nums[y+1];
                         //nums[y+1] = NULL;
                         nums[y+1] = -101;
@@ -89,23 +89,54 @@ public:
                 }//end of else if
                 //}
             }
-            printf("test_4\n");
-            printf("DEBUG FOR LOOP\n");
+            //printf("test_4\n");
+            printf("DEBUG FOR LOOP AT THE END\n");
             for (int z = 0; z < nums.size(); z++){
                 printf("%i ",nums[z]);
             }
             printf("\n\n");
-            printf("END OF DEBUG LOOP\n");
+            printf("END OF DEBUG LOOP AT THE END\n");
             //only_one = 0;
-            if (redo_once ==1){
-                redo_once = 0;
+            
+            // if (redo_once ==1){
+            //     redo_once = 0;
+            // }
+            // else if (redo_once ==0){
+            //     x=x-1;
+            //     redo_once = 1;
+            // }
+            
+            //if(redo_cnt )
+            
+            //if still have stuff == to x+1 or x-1 stay on that x
+            if (x+1 <=nums.size()-1){
+                
+                if (nums[x] == nums[x+1]){
+                printf("inside ending x+1 comp\n");
+                    
+                
+                if (x == 0){
+                    printf("if equal to zero\n");
+                    //x =0;//problem is still zero when added so it goes to 1
+                    x = -1;
+                }
+                
+                else{
+                    x -=1;//redo at this specific x since it'll be +1 so it comes
+                    
+                    }
+                }
+                //out to the same
             }
-            else if (redo_once ==0){
-                x=x-1;
-                redo_once = 1;
+            else if (x-1 >= 0){
+                if (nums[x] == nums[x-1]){
+                    x -=1;
+                }
             }
+            
         }//end of for loop
         printf("num_non_recur (at end) = %i",num_non_recur);
         return num_non_recur;
     }
 };
+//error is at -43 
