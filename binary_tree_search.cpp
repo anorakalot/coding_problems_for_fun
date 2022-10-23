7c30fc4b86eb944a670d5037c2e5a891e9c3b8a2
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-10-23 16:36:17 -0700

    wip #1 works for first test case
    fixed to work for first test case by having it so if return_val == val the left and right recursion searches just return that return_val instead of null

---
+++ binary_tree_search.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+	TreeNode *searchBST(TreeNode *root, int val)
+	{
+		TreeNode *return_val;
+		if (root->val == val)
+		{
+			printf("found root->val== val == %i\n", root->val);
+			return root;
+		}
+
+		// if (root->left != nullptr){
+		if (root->left != NULL)
+		{
+			printf("if root->left != null\n");
+			return_val = searchBST(root->left, val);
+			if (return_val->val == val)
+			{
+				return return_val;
+			}
+		}
+		if (root->right != nullptr)
+		{
+			printf("if root->right != null\n");
+			return_val = searchBST(root->right, val);
+			if (return_val->val == val)
+			{
+				return return_val;
+			}
+		}
+		return return_val;
+	}
+};
+// if (root->left == nullptr){
+//     return NULL;
+// }
+// if (root->right == nullptr){
+//     return NULL;
+// }
