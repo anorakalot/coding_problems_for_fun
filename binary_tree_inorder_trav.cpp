d6b6f1aa54a9e82ccfe77e28daae213f64cc3529
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-10-23 04:31:04 -0700

    wip #2 I figured out how the travesal should work but...
    can't seem to fgure out how to visit the parent node between recursive calling the left and right subtree.

--- binary_tree_inorder_trav.cpp
+++ binary_tree_inorder_trav.cpp
@@ -15,19 +15,52 @@ public:
 	vector<int> inorderTraversal(TreeNode *root)
 	{
 		vector<int> return_val;
-		if (root->left == nullptr)
-		{ // base case left subtree
-			// return root
-			return return_val;
-		}
-		if (root->right == nullptr)
-		{ // base case right subtree
-			return return_val;
+		// printf("val = %i\n ",root->val);
+		//  if (root->left == nullptr){//base case left subtree
+		//       //return root
+		//      return return_val;
+		//  }
+		//  if (root->right == nullptr){//base case right subtree
+		//      return return_val;
+		//  }
+
+		//         if (root->left != nullptr){
+		//             inorderTraversal(root->left);
+		//         }
+
+		//         // if (root->left == nullptr){
+		//         //     return_val.push_back(root->val);
+		//         // }
+		//         if (root->right != nullptr){
+		//             inorderTraversal(root->right);
+
+		//         }
+		// if (root->val == null){
+		//     return
+		// }
+
+		//         if (root->left == nullptr){
+		//             printf("val = %i",root->val);
+		//         }
+		//         inorderTraversal(root->left);
+
+		//         inorderTraversal(root->right);
+
+		//         return return_val;
+
+		// if (root->left == nullptr){
+		//     return_val.push_back(root->val);
+		// }
+		// return_val = inorderTraversal(root->left);
+		// return_val.push_back(root->val);
+		// return_val = inorderTraversal(root->right);
+
+		if (root != nullptr)
+		{
+			inorderTraversal(root->left);
+			inorderTraversal(root->right);
 		}
 
-		inorderTraversal(root->left);
-		inorderTraversal(root->right)
-
-			return return_val;
+		return return_val;
 	}
 };
