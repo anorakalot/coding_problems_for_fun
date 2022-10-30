1606d1bab9e29dbc1c1ad90fd6e2aa7078598446
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-10-30 00:40:09 -0700

    SUCCESS submit valid_pal works!

--- valid_palindrome.cpp
+++ valid_palindrome.cpp
@@ -4,22 +4,55 @@ public:
 	bool isPalindrome(string s)
 	{
 		int ascii;
+		string clone;
+
 		for (int x = 0; x < s.size(); x++)
 		{
 			ascii = s[x];
 			if (ascii <= 90 && ascii >= 65)
 			{ // if uppercase
-				printf("s[x] == %c is uppercase\n", s[x]);
-				printf("s[x] ascii value is ==  %i\n", ascii);
+				// convert to lowercase
+				//  printf("s[x] == %c is uppercase\n",s[x]);
+				//  printf("s[x] ascii value is ==  %i\n",ascii);
+				ascii += 32;
 				s[x] = s[x] + 32;
 			}
+			if (ascii >= 97 && ascii <= 122 || ascii >= 48 && ascii <= 57)
+			{
+				// if number or alphabet add to clone
+				clone.push_back(s[x]);
+			}
 		}
-
-		for (int x = 0; x < s.size(); x++)
+		// //debug clone print loop
+		// printf("debug clone print loop\n");
+		// for (int x = 0; x < clone.size(); x ++){
+		//     printf("%c",clone[x]);
+		// }
+		// printf("\n");
+		int first_index = 0;
+		int last_index = clone.size() - 1;
+		int return_val = 1;
+		while (first_index < last_index)
 		{
-			printf("%c", s[x]);
+			if (clone[first_index] != clone[last_index])
+			{
+
+				return_val = 0;
+				break;
+			}
+			first_index += 1;
+			last_index -= 1;
 		}
-		printf("\n");
-		return 0; // placeholder
+		// //comparison 2 pointers
+		// for (int x=0; x < s.size(); x++){
+		//     //if ()
+		// }
+
+		return return_val;
+		// return 0; //placeholder
 	}
 };
+// for (int x=0; x < s.size();x++){
+//              printf("%c",s[x]);
+//          }
+//          printf("\n");
