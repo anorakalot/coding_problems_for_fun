fa1d227abc05b34e32d81a52cb539cb669ed119b
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-11-01 17:04:06 -0700

    wip #3 it sorts but overflow bug ...
    it actually sort to 00 11  22 but there's a overflow bug butween 00 and 11 and 11 and 22 with the int value of -1094795586 
    after fixing this bug the code should be fine

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -5,8 +5,8 @@ public:
 	{
 		// low == 0 and high == nums.size()-1
 		// i think for starting value
-		printf("nums.size()-1 == %i\n", nums.size() - 1);
-		printf("nums[nums.size()-1] == %i\n", nums[nums.size() - 1]);
+		printf("nums.size()-1 == %i,nums[nums.size()-1] == %i\n", nums.size() - 1,
+			   nums[nums.size() - 1]);
 
 		quicksort(nums, 0, nums.size() - 1);
 	}
@@ -17,6 +17,7 @@ public:
 		if (low < high)
 		{
 			int pivot = partition(nums, low, high);
+			printf("returned_pivot_val == %i", pivot);
 			// recursion calling
 			quicksort(nums, low, pivot - 1);
 			// low to right before pivot
@@ -46,25 +47,34 @@ public:
 		printf("pivot == %i\n", pivot);
 		printf("pivot[nums] == %i\n", nums[pivot]);
 		int swap = 0;
-		for (int x = 0; x <= high - 1; x++)
+		for (int x = 0; x <= high; x++)
 		{
 			printf("nums[x] == %i\n", nums[x]);
 			printf("x == %i\n", x);
 			// j=x;
-			if (nums[x] < nums[pivot])
+			if (nums[x] <= nums[pivot])
 			{
 				printf("swapping\n");
 				printf("nums[x] == %i\n", nums[x]);
 				printf("nums[pivot] == %i\n", nums[pivot]);
 				i += 1; // increment i by 1
-				printf("nums[i] (after +=1) == %i\n", nums[i]);
-				printf("i == %i\n", i);
-				printf("\n");
-				// now swap i and j
-				swap = nums[x];
-				nums[x] = nums[i];
-				nums[i] = swap;
-			}
+				if (i == x)
+				{
+					// continue;
+					// if i is equal to x index
+					// then don't do anything
+				}
+				else if (x > i)
+				{
+					printf("nums[i] (after +=1) == %i\n", nums[i]);
+					printf("i == %i\n", i);
+					printf("\n");
+					// now swap i and j
+					swap = nums[x];
+					nums[x] = nums[i];
+					nums[i] = swap;
+				}
+			} // if (nums[x] <= pivot)
 			else
 			{
 				printf("not in nums[x] < nums[pivot]\n");
@@ -73,12 +83,15 @@ public:
 		// once at the end swap i+1 and pivot index
 		// then we return i+1 as where the pivot val is and this is where the recursion
 		// quicksort will then call them pivot -1 for lower and pivot +1 for higher
-		printf("Swapping nums[pivot] and nums[i+1]\n");
-		printf("i+1 = %i && nums[i+1] == %i\n", i + 1, nums[i + 1]);
-		printf("pivot = %i && nums[pivot] == %i\n", pivot, nums[pivot]);
-		swap = nums[pivot];
-		nums[pivot] = nums[i + 1];
-		nums[i + 1] = swap;
+
+		// printf("Swapping nums[pivot] and nums[i+1]\n");
+		// printf("i+1 = %i && nums[i+1] == %i\n",i+1,nums[i+1]);
+		// printf("pivot = %i && nums[pivot] == %i\n",pivot,nums[pivot]);
+		// swap = nums[pivot];
+		// nums[pivot] = nums[i + 1];
+		// nums[i + 1] = swap;
+		//(Comment)won't need above swap if x goes to high and does the last i+1 swap inside
+		// the loop
 
 		// reason see change is due to this swap here with pivot and i+1
 
@@ -87,9 +100,13 @@ public:
 		{
 			printf("%i ", nums[x]);
 		}
-		printf("\n");
-		printf("return_val of i+1 = %i\n", i + 1);
-		printf("nums[i+1] == %i\n", nums[i + 1]);
-		return (i + 1);
+		// printf("\n");
+		// printf("return_val of i+1 = %i\n",i+1);
+		// printf("nums[i+1] == %i\n",nums[i+1]);
+		// return (i + 1);
+
+		//(Comment)since last swap is already handled inside the loop
+		// should just need to return i
+		return i;
 	}
 };
