34a86bc04112ae1f7e8f384a0fb95708975eb592
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2022-11-02 21:19:56 -0700

    wip #4 reformat debug prints, think issue is i var
    Reformatted debug print statements and I think the issue is the return i values at the end because looking at the debug prints the return val at the end is 9 when it should not be any more than 5 the last index for this particular testcase array of 2,0,2,1,1,0 
    like the last one it does sort the given variables but with large negative numbers between them for some reason I don't fully know why yet.

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -10,6 +10,7 @@ public:
 
 		quicksort(nums, 0, nums.size() - 1);
 	}
+
 	void quicksort(vector<int> &nums, int low, int high)
 	{
 
@@ -17,11 +18,11 @@ public:
 		if (low < high)
 		{
 			int pivot = partition(nums, low, high);
-			printf("returned_pivot_val == %i", pivot);
+			printf("returned_pivot_val == %i\n", pivot);
 			// recursion calling
-			quicksort(nums, low, pivot - 1);
+			quicksort(nums, low, (pivot - 1));
 			// low to right before pivot
-			quicksort(nums, pivot + 1, high);
+			quicksort(nums, (pivot + 1), high);
 			// right after pivot to high
 		}
 		// else{//got rid of soo after recursion calls are done they just return
@@ -33,14 +34,24 @@ public:
 	}
 	int partition(vector<int> &nums, int low, int high)
 	{
+		printf("\n\n");
+		printf("low = %i, high = %i\n", low, high);
 		printf("nums before partition\n");
 		for (int x = 0; x <= high; x++)
 		{
 			printf("%i ", nums[x]);
 		}
 		printf("\n");
-		int i = low - 1; // so -1 if low was 0
-						 // which it should be for first partition
+		// int i = low - 1; // so -1 if low was 0
+		//  which it should be for first partition
+		int i = low;
+		// i -=1;
+		// stops here w/ new runtime error if i= low-1 or i-=1 2 line approach
+
+		// gonna try adding a int bool variable to keep i at low value once
+		// and skip i+= 1 once
+		int low_once = 0;
+		printf("i starting_val = %i,nums[i] = %i", i, nums[i]);
 		// int j;//don't need this if I have x for loop value
 		int pivot = high; // just make it the right most index value
 						  // for simplicity
@@ -57,7 +68,18 @@ public:
 				printf("swapping\n");
 				printf("nums[x] == %i\n", nums[x]);
 				printf("nums[pivot] == %i\n", nums[pivot]);
-				i += 1; // increment i by 1
+				// if (i != low){}
+				if (low_once == 0)
+				{ // first time i
+					// does not need to get incrememnted if start at i = low
+					low_once = 1;
+				}
+				else
+				{
+					i += 1; // increment i by 1
+				}
+				// i += 1; // increment i by 1
+
 				if (i == x)
 				{
 					// continue;
@@ -68,8 +90,8 @@ public:
 				{
 					printf("nums[i] (after +=1) == %i\n", nums[i]);
 					printf("i == %i\n", i);
-					printf("\n");
-					// now swap i and j
+					// printf("\n");
+					//  now swap i and j
 					swap = nums[x];
 					nums[x] = nums[i];
 					nums[i] = swap;
@@ -90,17 +112,19 @@ public:
 		// swap = nums[pivot];
 		// nums[pivot] = nums[i + 1];
 		// nums[i + 1] = swap;
+
 		//(Comment)won't need above swap if x goes to high and does the last i+1 swap inside
 		// the loop
 
-		// reason see change is due to this swap here with pivot and i+1
+		//(Ignore this I think)reason see change is due to this swap here with pivot and i+1
 
 		printf("nums after partition\n");
 		for (int x = 0; x <= high; x++)
 		{
 			printf("%i ", nums[x]);
 		}
-		// printf("\n");
+		printf("\n");
+
 		// printf("return_val of i+1 = %i\n",i+1);
 		// printf("nums[i+1] == %i\n",nums[i+1]);
 		// return (i + 1);
@@ -110,3 +134,8 @@ public:
 		return i;
 	}
 };
+// debugging help look at before and after partition loops
+// too see where error starts before going into detail
+
+// issue I see is that returned pivot val at the end is 9 when it shouldn't ever
+// go past 5 which is the last index for the array size for the test array 202110
