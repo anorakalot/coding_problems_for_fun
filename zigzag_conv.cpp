cc145fdaccd5b84ec09f79e052c114cf13943df4
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-08 14:59:20 -0800

    wip #2 working on making a algo for numColumns...
    Right now it doesn't give the right value for numRows = 5 which I hand calculated should be 6, also I made a if statement to handle the edgecase of s_size == 1 which it handles and just returns the string s and it passes that edge case on test 3.

--- zigzag_conv.cpp
+++ zigzag_conv.cpp
@@ -6,31 +6,54 @@ public:
 		printf("original string\n");
 		std::cout << s << std::endl;
 		int s_size = s.size();
-		printf("s_size == %i", s_size);
+		printf("s_size == %i\n", s_size);
+		printf("numRows == %i\n", numRows);
+
+		// edge case of s_size only being 1
+		if (s_size == 1)
+		{
+			return s;
+		}
 		// vector<vector<char>> zigzag_conv;
 
 		// int num_in_between = numrows/2;
 		// doesn't work for numrows 5 should be 3 but comes out to 2
-		int numColumns = 0;
+		int numColumns = 1; // since first increment when going to 2//(also always at least length 1)
 		int num_position = 0;
 		int direction = 0;
 
-		while (s_size <= 0)
+		// all of this to find numColumns
+		while (s_size > 0)
 		{
-			// if (direction == 0)
-			// {
+			if (direction == 0) // is set up to start dir as 0
+			{
+				if (num_position < (numRows - 1))
+				{
+					num_position += 1; // move up y
+					s_size -= 1;	   // used up a letter
+				}
+				else
+				{
+					direction = 1;
+				}
+			}
+			if (direction == 1)
+			{
+				if (num_position > 0)
+				{
+					num_position -= 1;
+					numColumns += 1;
+					s_size -= 1;
+				}
+				else
+				{
+					direction = 0;
+				}
+			}
 
-			//     if (num_position)
-			// }
-			if (direction == 0 && num_position < (numRows))
-			{
-				num_position += 1; // move up y
-				s_size -= 1;	   // used up a letter
-			}
-			else
-			{
-			}
-		}
+		} // end of while loop
+
+		printf("numColumns == %i\n", numColumns);
 
 		// should find mathematical way to get num of columns
 		// y      x
@@ -48,3 +71,31 @@ public:
 
 	} // end of convert class function
 };
+// //all of this to find numColumns
+// while (s_size <= 0){
+//     // if (direction == 0)
+//     // {
+
+//     //     if (num_position)
+//     // }
+//     if ((direction == 0) && (num_position<(numRows-1) )){
+//         num_position += 1;//move up y
+//         s_size -=1;//used up a letter
+//     }
+//     else{
+//         direction = 1;
+//         //after this else should go straight to next if statement
+//         //continue;
+//     }
+//     if ((direction ==1) && (num_position >(0))){
+//         num_position -=1;
+//         numColumns +=1;
+//         s_size -=1;
+//     }
+//     //I SEE THE PROBLEM if it goes here it never goes into the
+//     //second if statement
+//     else if {
+//         direction = 0;
+//     }
+
+// }
