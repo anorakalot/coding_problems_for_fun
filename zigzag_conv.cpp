1e416a5d198c30ee152e6b43c52c3b8df9d8429e
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-09 14:27:27 -0800

    wip #3 fixed numColumns bug...
    the error was s_size needed to be decremeneted by 1 due to the algorithm keeping track of the index which is always size -1 . I testsed on a different word "ROCKSTONE" with numRows 4 and it correctly gave 4 numColumns in my sub calc Col algorithm

--- zigzag_conv.cpp
+++ zigzag_conv.cpp
@@ -7,6 +7,9 @@ public:
 		std::cout << s << std::endl;
 		int s_size = s.size();
 		printf("s_size == %i\n", s_size);
+		printf("going to decrease s_size by 1 due to index error getting col error");
+		s_size -= 1;
+		printf("s_size == %i\n", s_size);
 		printf("numRows == %i\n", numRows);
 
 		// edge case of s_size only being 1
@@ -22,21 +25,30 @@ public:
 		int num_position = 0;
 		int direction = 0;
 
+		int s_index = 0;
+
 		// all of this to find numColumns
+		printf("starting_index = %c\n", s[s_index]);
 		while (s_size > 0)
 		{
+			// meaning it goes down
 			if (direction == 0) // is set up to start dir as 0
 			{
 				if (num_position < (numRows - 1))
 				{
 					num_position += 1; // move up y
 					s_size -= 1;	   // used up a letter
+					s_index += 1;
+					printf("going down current index = %c\n", s[s_index]);
+					printf("s_size = %i\n", s_size);
 				}
 				else
 				{
+					printf("changing direction to up\n");
 					direction = 1;
 				}
 			}
+			// going up zigzag means adding more columns
 			if (direction == 1)
 			{
 				if (num_position > 0)
@@ -44,9 +56,13 @@ public:
 					num_position -= 1;
 					numColumns += 1;
 					s_size -= 1;
+					s_index += 1;
+					printf("going up (meaning col +=1) current index = %c\n", s[s_index]);
+					printf("s_size = %i\n", s_size);
 				}
 				else
 				{
+					printf("changing direction to down\n");
 					direction = 0;
 				}
 			}
