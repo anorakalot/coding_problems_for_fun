7f6225da3e0381f416776e2cca3dc855e750fe47
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-10 16:55:41 -0800

    wip #4 added while loop to go/add to zigzag conv..
    there is an error where it's not printing out the debug loop at the end properly that I'll need to fix.  It does however print out right in the while loop when testing the proper y,x_vals and printing out zigzag_conv[y_val][x_val] in that loop

--- zigzag_conv.cpp
+++ zigzag_conv.cpp
@@ -70,19 +70,96 @@ public:
 		} // end of while loop
 
 		printf("numColumns == %i\n", numColumns);
+		printf("\n\n");
 
 		// should find mathematical way to get num of columns
 		// y      x
-		char zigzag_conv[numRows][numRows];
+		char zigzag_conv[numRows][numColumns];
+		string return_val;
+		int y_val = 0;
+		int x_val = 0;
 
-		// first convert to 2d array first not worrying about zigzag pattern
-		for (int y = 0; y < numRows; y++)
+		// could just do the same while loop as above but this time
+		// make it so that it adds numbers
+
+		// first reset s_size,direction,num_position,s_index var
+		s_size = s.size() - 1;
+		num_position = 0;
+		direction = 0;
+		s_index = 0;
+
+		printf("FILLING starting_index = %c\n", s[s_index]);
+		// add first letter to zigzag_conv
+		zigzag_conv[y_val][x_val] = s[s_index];
+		printf("added starting index to zigzag_conv at y_val = %i , x_val = %i\n", y_val, x_val);
+		printf("zigzag_conv[y_val][x_val] = %c\n", zigzag_conv[y_val][x_val]);
+		while (s_size > 0)
 		{
-			for (int x = 0; x < numRows; x++)
+			// meaning it goes down
+			if (direction == 0) // is set up to start dir as 0
 			{
+				if (num_position < (numRows - 1))
+				{
+					num_position += 1; // move up y
+					s_size -= 1;	   // used up a letter
+					s_index += 1;
+					// add 1 to y val
+					y_val += 1;
+					// keep x the same
+					// add to zigzag_conv;
+					zigzag_conv[y_val][x_val] = s[s_index];
+					printf("going down current index = %c\n", s[s_index]);
+					printf("added current index to zigzag_conv at y_val = %i , x_val = %i\n", y_val, x_val);
+					printf("zigzag_conv[y_val][x_val] = %c\n", zigzag_conv[y_val][x_val]);
+					printf("s_size = %i\n", s_size);
+				}
+				else
+				{
+					printf("changing direction to up\n");
+					direction = 1;
+				}
 			}
+			// going up zigzag means adding more columns
+			if (direction == 1)
+			{
+				if (num_position > 0)
+				{
+					num_position -= 1;
+					numColumns += 1;
+					s_size -= 1;
+					s_index += 1;
+					// y_val goes up (meaning decrease in 2d array)
+					y_val -= 1;
+					// increase x_val
+					x_val += 1;
+					zigzag_conv[y_val][x_val] = s[s_index];
+					printf("going up (meaning col +=1) current index = %c\n", s[s_index]);
+					printf("added current index to zigzag_conv at y_val = %i , x_val = %i\n", y_val, x_val);
+					printf("zigzag_conv[y_val][x_val] = %c\n", zigzag_conv[y_val][x_val]);
+					printf("s_size = %i\n", s_size);
+				}
+				else
+				{
+					printf("changing direction to down\n");
+					direction = 0;
+				}
+			}
+
+		} // end of FILLING zigzag_conv while loop
+
+		// put debug print loop here
+		printf("DEBUG LOOP\n");
+		for (int y = 0; y < numRows - 1; y++)
+		{
+			for (int x = 0; x < numColumns - 1; x++)
+			{
+				printf("testing\n");
+				printf("%c ", zigzag_conv[y][x]);
+			}
+			printf("\n");
 		}
 
+		// actually ending might be a double for loop to go through and only add the values in a linear fashion after makign a zigzag pattern
 		return "0";
 
 	} // end of convert class function
