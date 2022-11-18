2b064600120a537c2c72b7e2406e91e7ec3e13e7
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-17 17:32:10 -0800

    wip #3 added num_of_perms and num_of_curr_perms...
    to the recur functions arguments to be used in that function to determine when the total number of possible permutations has been found.

--- permutations.cpp
+++ permutations.cpp
@@ -4,16 +4,17 @@ public:
 	vector<vector<int>> permute(vector<int> &nums)
 	{
 		vector<vector<int>> return_val;
-		int num_of_permutations;
-		num_of_permutations = nums.size(); // start off 3
-		printf("start_val num_of_permutations = %i\n", num_of_permutations);
-		printf("nums.size()-1 = %i\n", nums.size() - 1);
+		int num_of_perms;
+		num_of_perms = nums.size(); // start off 3
+		printf("start_val num_of_perms = %i\n", num_of_perms);
+		printf("nums.size()-1 = %lu\n", nums.size() - 1);
 		for (int x = nums.size() - 1; x > 0; x--)
 		{
 			printf("x == %i \n", x);
-			num_of_permutations *= x;
+			num_of_perms *= x;
 		}
-		printf("num_of_permutations = %i\n", num_of_permutations);
+		printf("num_of_perms = %i\n", num_of_perms);
+		int num_of_curr_perms = 0;
 		// testing_pushback
 		//  vector<int> test_1= {5,1,6,7,2,1};
 		//  vector<int> test_2 = {10,5,4,1};
@@ -27,14 +28,16 @@ public:
 		//  }
 		////
 		map<vector<int>, int> been_done_before;
-		return_val = permute_recur(nums, been_done_before, return_val);
+		return_val = permute_recur(nums, been_done_before, return_val, num_of_perms, num_of_curr_perms);
 		return return_val;
 	}
-	vector<vector<int>> permute_recur(vector<int> &nums, map<vector<int>, int> been_done_before, vector<vector<int>> return_val)
+	vector<vector<int>> permute_recur(vector<int> &nums, map<vector<int>, int> been_done_before, vector<vector<int>> return_val,
+									  int num_of_perms, int num_of_curr_perms)
 	{
+
 		if (been_done_before[nums] == 0)
 		{ // if nums hasn't been done_before
-			printf("UNIQUE PERMUTE NUMS\n");
+			printf("UNIQUE PERM NUMS VECTOR \n");
 			for (int x = 0; x < nums.size(); x++)
 			{
 				printf("%i ", nums[x]);
@@ -42,6 +45,11 @@ public:
 			printf("\n");
 			return_val.push_back(nums);
 			been_done_before[nums] = 1; // set it to 1 so that we know this permutation has already been done
+			num_of_curr_perms += 1;
+			if (num_of_curr_perms >= num_of_perms)
+			{ // found the total number of possible permutations
+				return return_val;
+			}
 		}
 
 		return return_val;
