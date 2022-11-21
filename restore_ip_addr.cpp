2aaec17ed42d8822fbd9f250da91ba707ec8d9c1
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-21 05:10:19 -0800

    wip restore_ip_addr
    trying to set up recursion for backtracking,

---
+++ restore_ip_addr.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+	vector<string> restoreIpAddresses(string s)
+	{
+		// if (s == )//base case here
+
+		vector<string> return_val;
+
+		return_val = restore_ip_addr_recur(s, 0);
+	}
+	vector<string> restore_ip_addr_recur(string s, int index)
+	{
+	}
+};
