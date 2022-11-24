0800761e9fc1a7af1bcc7cb229bd500657005620
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-23 16:46:15 -0800

    wip backtrack func not working correctly

--- permutations.cpp
+++ permutations.cpp
@@ -4,8 +4,9 @@ public:
 	vector<vector<int>> permute(vector<int> &nums)
 	{
 		vector<vector<int>> return_val;
-		int num_of_perms;
-		num_of_perms = nums.size(); // start off 3
+		map<vector<int>, int> been_done_before;
+
+		int num_of_perms = nums.size(); // start off 3
 		printf("start_val num_of_perms = %i\n", num_of_perms);
 		printf("nums.size()-1 = %lu\n", nums.size() - 1);
 		for (int x = nums.size() - 1; x > 0; x--)
@@ -15,43 +16,42 @@ public:
 		}
 		printf("num_of_perms = %i\n", num_of_perms);
 		int num_of_curr_perms = 0;
-		// testing_pushback
-		//  vector<int> test_1= {5,1,6,7,2,1};
-		//  vector<int> test_2 = {10,5,4,1};
-		//  return_val.push_back(test_1);
-		//  return_val.push_back(test_2);
-		//  for (int y = 0; y < 2; y++){
-		//      for (int x = 0; x < return_val[y].size(); x++){
-		//          printf("%i ",return_val[y][x]);
-		//      }
-		//      printf("\n");
-		//  }
-		////
-		map<vector<int>, int> been_done_before;
-		return_val = permute_recur(nums, been_done_before, return_val, num_of_perms, num_of_curr_perms);
+		vector<int> permutation;
+		map<int, int> used_num_in_permutation;
+		permute_backtrack_recur(nums, permutation, used_num_in_permutation, return_val); // num_of_perms,num_of_curr_perms);
+
 		return return_val;
 	}
-	vector<vector<int>> permute_recur(vector<int> &nums, map<vector<int>, int> been_done_before, vector<vector<int>> return_val,
-									  int num_of_perms, int num_of_curr_perms)
+	// vector<vector<int>> make backtrack void already I save the return_val as a function parameter
+	void permute_backtrack_recur(vector<int> &nums, vector<int> &permutation, map<int, int> used_num_in_permutation, vector<vector<int>> return_val)
 	{
+		if (permutation.size() == nums.size())
+		{
+			// add to return_val;
+			return_val.push_back(permutation);
+			return;
+		}
 
-		if (been_done_before[nums] == 0)
-		{ // if nums hasn't been done_before
-			printf("UNIQUE PERM NUMS VECTOR \n");
-			for (int x = 0; x < nums.size(); x++)
+		for (int x = 0; x < nums.size(); x++)
+		{
+			if (used_num_in_permutation[nums[x]] == 0)
 			{
-				printf("%i ", nums[x]);
-			}
-			printf("\n");
-			return_val.push_back(nums);
-			been_done_before[nums] = 1; // set it to 1 so that we know this permutation has already been done
-			num_of_curr_perms += 1;
-			if (num_of_curr_perms >= num_of_perms)
-			{ // found the total number of possible permutations
-				return return_val;
+				permutation.push_back(nums[x]);													 // apply choices
+				used_num_in_permutation[nums[x]] = 1;											 // just do this so we know we already used this val
+				permute_backtrack_recur(nums, permutation, used_num_in_permutation, return_val); // backtrack
+				int index_to_erase = 0;
+				for (int x = 0; x < permutation.size(); x++)
+				{ // undo choice
+					if (permutation[x] == nums[x])
+					{
+						permutation.erase(permutation.begin() + index_to_erase);
+						break;
+					}
+					index_to_erase += 1;
+				}
 			}
 		}
 
-		return return_val;
+		return;
 	}
 };
