fda14380032a21181f97dcae9e768a9d1431cced
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-11-27 14:04:57 -0800

    wip added memoization but there's a bug during compile

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,9 +1,10 @@
 class Solution
 {
 public:
-	int fib(int n, vector<int> already_done)
+	int fib(int n, map<int, int> &already_done)
 	{
-
+		// vector<int>& already_done //I'm gonna use a map instead
+		// 2 base cases (1,0)
 		if (n == 1)
 		{
 			return 1;
@@ -12,7 +13,17 @@ public:
 		{
 			return 0;
 		}
-
-		return fib(n - 1) + fib(n - 2);
+		int return_val;
+		if (already_done[n] != 0)
+		{ // already done
+			return_val = already_done[n];
+			return return_val;
+		}
+		return_val = fib(n - 1, already_done) + fib(n - 2, already_done);
+		if (already_done[n] == 0)
+		{
+			already_done[n] = return_val;
+		}
+		return return_val;
 	}
 };
