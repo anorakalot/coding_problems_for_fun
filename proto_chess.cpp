ee94dde8c4077515f4edfa3e6fd659470607b5ba
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-12-09 22:12:23 -0800

    wip finsihes setup_board and print_board...
    functions, now to start on the basic game logic

--- proto_chess.cpp
+++ proto_chess.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 using namespace std;
 
-void print_board(char chess_board[8][8]);
+char inital_piece_setup[8] = {'r', 'k', 'b', 'Q', 'K', 'b', 'k', 'r'};
 
+void print_board(char (&chess_board)[8][8]);
+void setup_initial_board(char (&chess_board)[8][8]);
 class piece
 {
 public:
@@ -14,27 +16,53 @@ public:
 int main()
 {
 	printf("WELCOME TO CHESS\n");
-	printf("test_1\n");
 	// vector<vector<char>> chess_board('-');
-	char chess_board[8][8];
-	printf("test_2\n");
-	chess_board[1][5] = 'A'; // error is here  so I think it's because I didn't set up size of vector
-	printf("test_3\n");
+	char chess_board[8][8] = {'-'};
+	for (int y = 0; y < 8; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			chess_board[y][x] = '-';
+		}
+	}
+	// chess_board[1][5] = {'P'}; // error is here  so I think it's because I didn't set up size of vector
 	printf("right before print_board func\n");
 	print_board(chess_board);
 	printf("after print_board func\n");
 
+	setup_initial_board(chess_board);
+	print_board(chess_board);
+
 	return 0;
 }
 
-void print_board(char chess_board[8][8])
+void print_board(char (&chess_board)[8][8])
 {
-	printf("Right before loop");
-	for (int y = 0; y < 0; y++)
+	printf("Right before loop\n");
+	for (int y = 0; y < 8; y++)
 	{
 		for (int x = 0; x < 8; x++)
 		{
-			printf("%c", chess_board[y][x]);
+			printf(" %c ", chess_board[y][x]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+void setup_initial_board(char (&chess_board)[8][8])
+{
+	for (int y = 0; y < 8; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			if (y == 0 || y == 7)
+			{
+				chess_board[y][x] = inital_piece_setup[x];
+			}
+			else if (y == 1 || y == 6)
+			{
+				chess_board[y][x] = 'p';
+			}
 		}
 	}
 }
