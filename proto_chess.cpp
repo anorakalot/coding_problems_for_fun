52f41cd7f3966ba2aadd1fac911ebb9c75504e8a
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-12-12 18:22:20 -0800

    wip chess in c++ and wip tic tac toe in ADA

--- proto_chess.cpp
+++ proto_chess.cpp
@@ -3,21 +3,53 @@
 #include <vector>
 using namespace std;
 
+// need to figure out how to set up class object pieces
+
 char inital_piece_setup[8] = {'r', 'k', 'b', 'Q', 'K', 'b', 'k', 'r'};
 
 void print_board(char (&chess_board)[8][8]);
 void setup_initial_board(char (&chess_board)[8][8]);
+char chess_board[8][8] = {'-'};
+
 class piece
 {
 public:
-	void move();
+	int y_coord;
+	int x_coord;
+	int y_input;
+	int x_input;
+	vector<pair<int, int>> possible_moves; // make a vector of possilbe moves each time a piece is selected
+
+	virtual void move();
+	virtual void find_curr_avail_moves();
+};
+
+class pawn : public piece
+{
+	void find_curr_avail_moves()
+	{
+		possible_moves.clear();
+		possible_moves.shrink_to_fit();
+
+		// should find a more algorithmic way to do this for future pieces like bishops , rooks , queens
+		possible_moves.push_back(make_pair(y_coord - 1, x_coord - 1)); // this is y-1 because -1 goes up and -1 x goes to the left
+		possible_moves.push_back(make_pair(y_coord - 1, x_coord + 1))  // again going up -1 for y and +1 for x goes to the right
+	}
+	void move()
+	{
+		// printf("possible moves are (%i,%i) and (%i,%i)", y_coor)
+		// instead of printing out possible moves make it so ask for coord input
+		scanf("y_input %i ", &y_input);
+		scanf("x_input %i ", &x_input);
+
+		// then check if input is in possilbe moves vector list
+	}
 };
 
 int main()
 {
 	printf("WELCOME TO CHESS\n");
 	// vector<vector<char>> chess_board('-');
-	char chess_board[8][8] = {'-'};
 	for (int y = 0; y < 8; y++)
 	{
 		for (int x = 0; x < 8; x++)
@@ -26,9 +58,9 @@ int main()
 		}
 	}
 	// chess_board[1][5] = {'P'}; // error is here  so I think it's because I didn't set up size of vector
-	printf("right before print_board func\n");
-	print_board(chess_board);
-	printf("after print_board func\n");
+	// printf("right before print_board func\n");
+	// print_board(chess_board);
+	// printf("after print_board func\n");
 
 	setup_initial_board(chess_board);
 	print_board(chess_board);
@@ -66,3 +98,20 @@ void setup_initial_board(char (&chess_board)[8][8])
 		}
 	}
 }
+
+// ////proto switch case statmeents
+// enum CHESS_STATES = {chess_init, chess_start, chess_move, chess_valid_y_n, chess_check} chess_state;
+
+// switch (chess_state)//transitions
+// {
+// 	case chess_init;
+// 	break;
+
+// };
+
+// switch(chess_state)//actions
+// {
+// 	case chess_init;
+// 	break;
+
+// }
--- tic_tac_toe_program.adb
+++ tic_tac_toe_program.adb
@@ -1,27 +1,49 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Tic_Tac_Toe_Program is
-   input: Integer :=0;
+   --input: Integer :=0; -- thjios is why there was a confl of decl error was beacuse this variable was
+   --already being used for this input.
    --  X: Character := 'X';
-
-   type X_OR_O is (X,O);
+   type X_OR_O is (X,O,NA);--error was here because I think that - is a special characer
+   --so for now just using NA
+   type game_over is  range 0..1;
    type y_col_index is range 0 .. 2;
    type x_row_index is range 0 .. 2;
 
    type Tic_tac_toe_board is array (y_col_index,x_row_index) of X_OR_O;
-
+   board : Tic_tac_toe_board := ((NA,NA,NA),(NA,NA,NA),(NA,NA,NA));
+   input : X_OR_O := X;
 begin
    --  Insert code here.
    for y in y_col_index loop
       --Put_line("test loop!" & Integer'Image(y));
       for x in x_row_index loop
-      Put("y " & y_col_index'Image(y) & " x " &
-              x_row_index'Image(x) & " ");
+--         input := X;
+         board(y,x) := input ;
+         Put(X_OR_O'Image(board(y,x)) & " ");
+         --  board(y,x) := -;
+         --  Tic_tac_toe_board(y,x) := -;
+         --  put (Tic_tac_toe_board'Image(y,x) & " ");
 
+         --  Put(tic_tac_toe_board);
       end loop;
       New_line;
-      end loop;
+   end loop;
    --  /Put_Line ("hello world");
 
 
 end Tic_Tac_Toe_Program;
+
+--
+--  for y in y_col_index loop
+--        --Put_line("test loop!" & Integer'Image(y));
+--        for x in x_row_index loop
+--        Put("y " & y_col_index'Image(y) & " x " &
+--                x_row_index'Image(x) & " ");
+--
+--        end loop;
+--        New_line;
+--     end loop;
+
+
+-- prototype case for tic tac toe program
