788717a000c1b715bdf32cfe3a3fd787a0c45e01
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2022-12-18 23:13:04 -0800

    wip finished prototype transition FSM...
    still have to do prototype actions FSM and then test , also need to make sure check win algorithm is good performance wise

--- tic_tac_toe_program.adb
+++ tic_tac_toe_program.adb
@@ -20,6 +20,10 @@ procedure Tic_Tac_Toe_Program is
                        CHOOSE_MOVE,CHECK_IF_VALID_MOVE,CHECK_WIN,
                        SWITCH_PLAYER,GAME_OVER);
    game_state : GAME_STATES := GAME_INIT;
+   type valid_bool is range 0 .. 1;
+   valid_move_bool : valid_bool :=0;
+   valid_win_bool : valid_bool := 0;
+
    procedure display_board is
    begin
       for y in y_col_index loop
@@ -46,7 +50,42 @@ begin
    --  Insert code here.
    --put main switch case finite state machine here
    display_board;
+   case game_state is --transitions
+      when GAME_INIT =>
+         game_state := GAME_START;
+      when GAME_START =>
+         game_state := CHOOSE_X_O;
+      when CHOOSE_X_O=>
+         game_state := CHOOSE_MOVE;
+      when CHOOSE_MOVE=>
+         game_state := CHECK_IF_VALID_MOVE;
+      when CHECK_IF_VALID_MOVE =>
+         if valid_move_bool = 0 then
+            game_state := CHOOSE_MOVE; --goes back to choose another move
+         elsif valid_move_bool = 1 then
+            game_state := CHECK_WIN;
+         else
+            Put_Line("ERROR IN CHECK_IF_VALID_MOVE");
+         end if ;
+      when CHECK_WIN=>
+         if valid_win_bool = 0 then
+            game_state := SWITCH_PLAYER;
+         elsif valid_win_bool = 1 then
+            game_state := GAME_OVER;
+         else
+            Put_Line("Error in CHECK_WIN");
+         end if;
+      when SWITCH_PLAYER=>
+         game_state := CHOOSE_MOVE;
+      when GAME_OVER=>
+         --nothing should just stay in GAME_OVER
+         game_state := GAME_OVER;--might need to change this looping back to game over state
+         --  end case
+   end case;
 
+  -- case game_state is --actions
+    --  when
+   --end case;
 
 
 end Tic_Tac_Toe_Program;
