97e614db03a1155ba9ca0558529278ac0cdc58cb
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2023-01-05 23:30:32 -0800

    Success reverse linked list problem
    this version has the print statements for clarity but the submission that had 0ms on my leetcode submission was the same code just had all the print statements taken out

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -8,22 +8,45 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* prev_head;
-    ListNode* reverseList(ListNode* head) {
-        if (head== NULL){
-            return head;
-        }
-        if (head->next == nullptr){
-            return head;
-        }
+	ListNode *prev_head;
+	ListNode *head_to_return;
+	int total_nodes = 0;
+	ListNode *reverseList(ListNode *head)
+	{
+		if (head == NULL)
+		{
+			head_to_return = head; // because this is the last head;
+			return head;
+		}
+		if (head->next == nullptr)
+		{
+			head_to_return = head; // because this is the last head;
+			return head;
+		}
+		total_nodes += 1;
+		prev_head = reverseList(head->next);
 
-        prev_head = reverseList(head->next);
-        prev_head->next = head;
-        printf("prev_head->val == %i prev_head->next->val == %i\n",prev_head->val,prev_head->next->val);
+		// head->next = nullptr;
+		printf("test_1\n");
+		prev_head->next = head;
+		printf("head->val == %i\n", head->val);
+		printf("total_nodes== %i\n", total_nodes);
+		printf("prev_head->val == %i prev_head->next->val == %i\n", prev_head->val, prev_head->next->val);
 
-        //return prev_head;
-        return head;//just a placeholder to make sure recursive calls work
-    }
+		total_nodes -= 1;
+		// return prev_head;
+		printf("total_nodes right before if== %i \n", total_nodes);
+		if (total_nodes == 0)
+		{
+			head->next = nullptr;
+			return head_to_return;
+		}
+		else
+		{
+			return head; // just a placeholder to make sure recursive calls work
+		}
+	}
 };
