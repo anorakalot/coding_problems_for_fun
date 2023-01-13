503bab80a54c5d3c41dccd011e85913e3d4430ba
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2023-01-12 16:09:40 -0800

    Success power of three

--- power_of_three.cpp
+++ power_of_three.cpp
@@ -1,25 +1,29 @@
-class Solution {
+class Solution
+{
 public:
-    int x = 0;
-    int val = 0;
-    bool return_val;
-    bool isPowerOfThree(int n) {
-        if (n ==0){
-            return 0;
-        }
-        if (val > n){
-            return 0;
-        }
-        else if (val==n){
-            return 1;
-        }
+	unsigned long x = 0;
+	unsigned long val = 0;
+	bool return_val;
+	bool isPowerOfThree(int n)
+	{
+		if (n <= 0)
+		{
+			return 0;
+		}
+		if (val > n)
+		{
+			return 0;
+		}
+		else if (val == n)
+		{
+			return 1;
+		}
 
-        //x+=1;
-        val = pow(3,x);
-        x+=1;
-        return_val = isPowerOfThree(n);
+		// x+=1;
+		val = pow(3, x);
+		x += 1;
+		return_val = isPowerOfThree(n);
 
-        return return_val;
-
-    }
+		return return_val;
+	}
 };
