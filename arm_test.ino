e0c5f9f2ecf23076c3a5fa63fee30488e2da80a8
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2023-02-05 16:53:03 -0800

    wip arm_test

--- arm_test.ino
+++ arm_test.ino
@@ -2,6 +2,12 @@
 Servo myservo_0;//base set to 90 initially
 Servo myservo_1;//find base angle =
 Servo myservo_2;//find base angle = 
+//changed from float to int because servo.write functions uses int
+void inv_kin(int x, int y, int z);//take in wanted positiong change angle
+//make angle for myservo objects global to make it easier
+int angle_0 = 90;
+int angle_1 = 60; 
+int angle_2 = 130;
 
 int increment = 10;
 void setup() {
@@ -23,17 +29,36 @@ void loop() {
 //  myservo.write(90);
 //  delay(1000);
 
-  for (int x = 0; x < 90; x+=increment){
-    myservo_1.write(x);    
-    delay(1000);
-  }
+//  for (int x = 0; x < 90; x+=increment){
+//    myservo_1.write(x);    
+//    delay(1000);
+//  }
+//
+//  for (int x = 90; x >0; x -= increment){
+//    myservo_1.write(x);
+//    delay(1000);
+//  }
 
-  for (int x = 90; x >0; x -= increment){
-    myservo_1.write(x);
-    delay(1000);
-  }
+//  myservo_1.write(60);
+//  delay(1000);
   //    myservo_1.write(0);
 //    delay(1000);
 //    myservo_1.write(90);
 //    delay(1000);
+
+myservo_1.write(40);
+delay(100);
+
+//  myservo_2.write(130);
+//  delay(1000);
+//    for (int x = 60; x <130; x += increment){//starting at 0 leads to it not moving for a while so start at a higher num for this servo
+//    myservo_2.write(x);
+//    delay(1000);
+//  }
+}
+
+void inv_kin(int x, int y, int z){
+
+
+  return; 
 }
