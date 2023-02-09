e4507540e56d5a17c805ba0b2a5390ecc9c8ad56
Richard "Dylan" Onate McGee <sleepyanorak@gmail.com>
2023-02-09 13:36:12 -0800

    wip lab_5 ada_book

---
+++ lab_5_book.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO;
+with Ada.Strings.Bounded;
+procedure Lab_5_Book is
+   type company_record is record
+      --first_name : Ada.Strings.Bounded.Bounded_String(1..50);
+      --first_name : String(1..50);
+      first_name : Bounded_string;
+      --last_name : String(1..50);
+      end record;
+begin
+    --  Insert code here.
+
+   null;
+end Lab_5_Book;
