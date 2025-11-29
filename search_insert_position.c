f4ab4131a511dc1cd07dbab0fe506878be795b11
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-11-29 13:30:36 -0800

    41/66 test cases passed and all of the default test cases passed

--- search_insert_position.c
+++ search_insert_position.c
@@ -7,42 +7,48 @@ int searchInsert(int* nums, int numsSize, int target) {
     int high = numsSize - 1;
     int middle = 0;
 
+    printf("numsSize = %i\n",numsSize);
+
     int counter = 0;
 
     while( nums[middle] != target ) { 
     
-    printf("counter = %i\n",counter);
+        printf("counter = %i\n",counter);
 
-    middle = (high + low) / 2;
-    printf("middle = %i\n ",middle);
+        printf("low = %i\n", low);
+        printf("high = %i\n", high);
+        middle = (high + low) / 2;
+        printf("middle = %i\n ",middle);
 
-    printf("nums[middle] = %i\n ",nums[middle]);
+        printf("nums[middle] = %i\n",nums[middle]);
 
-    if (nums[middle] > target && !(middle >=numsSize-1)){
-    printf("nums[middle] > target\n");
-    high = middle;
-    }
+        if (nums[middle] > target && !(middle >=numsSize-1) && low < high){
+        printf("nums[middle] > target\n\n");
+        high = middle - 1;
+        }
 
-    else if( nums[middle] < target && middle != 0){
-    printf("nums[middle] < target\n");
-    low = middle;
-    }
+        else if( nums[middle] < target && middle != 0 && low < high){
+        printf("nums[middle] < target\n\n");
+        low = middle + 1;
+        }
 
-    else if (middle == 0){
-        middle += 1;
-        return middle;
-    }
+        else if (middle == 0){
+            printf("middle == 0\n\n");
+            middle += 1;
+            return middle;
+        }
 
-    else if (middle >= (numsSize-1)){
-        middle +=1;
-        return middle;
-    }
+        else if (middle >= (numsSize-1)){
+            printf("middle >= numsSize-1\n\n");
+            middle +=1;
+            return middle;
+        }
 
-    else{
-        return middle;
-    }
+        else{
+            return middle;
+        }
 
-    counter += 1;
+        counter += 1;
     }
     return 0;
 }
