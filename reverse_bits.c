7d4b2c5b1367d46099581f8405ccbdab3e4cdc89
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-11-30 15:01:37 -0800

    new comment on way to solve problem

--- reverse_bits.c
+++ reverse_bits.c
@@ -1,17 +1,4 @@
 int reverseBits(int n) {
-    //ok so the first thing is how to convert to binary
-    int counter = 0;
-
-    //have something that goes from 2^n to 2^n-1 to 2^n-2 all the way to 2^0
-
-    printf("n= %i \n",n);
-    //then in here we would make it so that just convert it to not? 
-
-    //or maybe we don't need to convert to binary
-
-
-    // int result = 
-    // return ~(n);//this doesn't work just returns negative value
 
     //oh wait it means reverse in terms of splitting down the middle and replaceing each side
 
@@ -21,8 +8,15 @@ int reverseBits(int n) {
 
     first_half = (0xFFFF0000 & n) >> 16;
 
-    second_half = (0x0000FFFF) & n << 16;
+    printf("first_half = %x\n",first_half);
 
-    
+    second_half = (0x0000FFFF & n); //<< 16;
+
+    printf("second_half = %x\n",second_half);
+
+//oh wait we'd need multiple different halfs because the binary is not 
+
+//or maybe something else like go through each bit in a for loop and save it in a different int with a 0x80000000
+
     return 0;
 }
