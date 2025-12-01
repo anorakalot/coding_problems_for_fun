5c6531492d3554bfb0d9559a8fb1f112dc885d53
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-11-30 17:26:09 -0800

    more wip reverse_bits

--- reverse_bits.c
+++ reverse_bits.c
@@ -5,12 +5,26 @@ int reverseBits(int n) {
 //or maybe something else like go through each bit in a for loop and save it in a different int with a 0x80000000
 
     int return_val = 0x00000000;
-    int mask = 0x00000001;
+    int mask_for_return_val = 0x00000000;
+    int mask_for_n = 0x00000001;
     bool bit_val_n = 0;
 
-    for (int x = 0; x < 32: x++){
+    int left_shift_for_return_val = 31;
+
+    for (int x = 0; x < 32; x++){
         //first get the first 2^0 bit value of n
-        bit_val_n = mask & n;
+        bit_val_n = mask_for_n & n;
+        printf("bit_val = %i\n",bit_val_n);
+        printf("mask = %x\n",mask_for_n);
+        mask_for_n <<= 1;
+
+        return_val |= (bit_val_n << left_shift_for_return_val);
+
+        printf("return_val = %x\n",return_val);
+        // return_val |= (bit_val_n << 32);
+
+
+        left_shift_for_return_val -=1;
 
         //then move the bit_val_n to return_val 0x80000000 position 
         //so maybe left shift it by a ton?
