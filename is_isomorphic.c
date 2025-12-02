541a2a14a6e30d0822e833a1ba5b3c9140aade78
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-02 12:51:23 -0800

    Add files via upload

--- is_isomorphic.c
+++ is_isomorphic.c
@@ -1,6 +1,6 @@
 bool isIsomorphic(char* s, char* t) {
     //ok so isIsomorphic
-    means 
+    //means 
 
     //for loop
     // isomorphic check if
@@ -8,4 +8,30 @@ bool isIsomorphic(char* s, char* t) {
     //what's the non case for this maybe do that
     //it seems to be if they're not the same size
     //and if there's any two repeating numbers in input s?
+
+
+    //ok so maybe for loops check until it's '\0' string?
+
+    int index_s = 0;
+    int index_t_ = 0;
+
+    int s_size = 0;
+    int t_size = 0;
+
+    while(s[index_s] != '\0'){
+        
+        index_s += 1;
+        s_size +=1;
+    }
+    while(t[index_t_] != '\0'){
+        index_t_ += 1;
+        t_size +=1;
+    }
+
+    printf("s_size = %i\n",s_size);
+    printf("t_size = %i\n",t_size);
+
+
+    return 0;
+
 }
