61ec96ec642449a529d3bc341d54cbc9110039d5
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-01 17:54:04 -0800

    first wip is isomorphic

---
+++ is_isomorphic.c
@@ -0,0 +1,11 @@
+bool isIsomorphic(char* s, char* t) {
+    //ok so isIsomorphic
+    means 
+
+    //for loop
+    // isomorphic check if
+
+    //what's the non case for this maybe do that
+    //it seems to be if they're not the same size
+    //and if there's any two repeating numbers in input s?
+}
