f781b645af7975ea718d591fe45f4f2342c49bed
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-03 15:36:33 -0800

    first wip summary_ranges

---
+++ summary_ranges.c
@@ -0,0 +1,14 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** summaryRanges(int* nums, int numsSize, int* returnSize) {
+    
+    //go through the array for loop
+    //if the array is >0 and the array index is
+
+
+    //ok so the biggest thing is checking either after the current index or before the current index;
+    //checking after erases needing to do a skip i = 0 method
+    myString = (char *)malloc(sizeof(char) * (3 + 1));
+
+}
