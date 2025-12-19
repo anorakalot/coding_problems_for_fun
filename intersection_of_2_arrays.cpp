0dc25f4b26748112c8fdf4235fa5027e955682b6
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-19 14:36:01 -0800

    changed to hash table implementation

---
+++ intersection_of_2_arrays.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> return_arr;
+
+        return_arr.push_back();
+
+        //probably do a hash table
+        unordered_map<int, int> unique_val_map;
+
+        unique_val_map.contains()
+    }
+};
+
+
+//c version below
+// /**
+//  * Note: The returned array must be malloced, assume caller calls free().
+//  */
+// int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    
+//     //due to malloc being dependent on the final answer maybe don't allocate it until the very end?
+//     //or maybe allocate it with nums1size + nums2size?
+//     int *return_array;
+//     return_array = malloc((the size of the array) * sizeof(*return_array));
+
+//     //so anything that shows up in both
+
+//     //the simplest soln is to do 0n^2 loopfor each one in the smallest array loop through the 
+//     //other array checking if any of the values in it are equal then add that to the return array
+//     // probably don't want to do that
+
+
+// }
