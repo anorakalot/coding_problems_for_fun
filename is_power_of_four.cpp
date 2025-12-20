6e1e76b9fdfb022a72ceffe7b1ffd5125cc3e6b6
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-20 00:20:45 -0800

    882 out of 1063 test cases passed

--- is_power_of_four.cpp
+++ is_power_of_four.cpp
@@ -5,11 +5,19 @@ public:
         //maybe right shift by 2 and see if it's 1
         //1 0000  >> 2 0 0100 >> 2 0001
 
+        // 0101
+
+        //not even
+        if (n % 2 != 0 && n != 1){
+            return 0;
+        }
+
         while ( n > 0){
             if (n == 1){
                 return 1;
             }
             n >>= 2;
+            std::cout<< n << std::endl;
         }
 
         return 0;
