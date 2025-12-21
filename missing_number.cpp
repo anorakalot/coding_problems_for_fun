7499d594b66807cf7ae12bc94094cb9259e58f5d
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-20 22:26:00 -0800

    missing_number first wip

---
+++ missing_number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        //maybe the solution relies on filling out each bit?
+
+
+        //maybe loop through each one
+
+        //it seems it's how many are in the nums.size()-1 
+
+        std::cout<<(nums.size())<< std::endl;
+        
+        //you could also just do a hash map
+
+        //or maybe it's soemthing with 9
+
+        return 0;
+    }
+};
