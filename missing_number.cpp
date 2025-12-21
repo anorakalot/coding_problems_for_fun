acdc66adfbc13954e97225173219785758b28271
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-21 07:52:29 -0800

    wip towards soln that's vector based

--- missing_number.cpp
+++ missing_number.cpp
@@ -14,6 +14,21 @@ public:
 
         //or maybe it's soemthing with 9
 
+        //ok could also do something like have a vector initialize all to zeros and if that number is in nums
+        //add a 1 at that nums index then do another for loop through it
+        vector<int> checking_values(nums.size(),0);
+        
+        for (int x = 0; x < nums.size(); x++){
+            checking_values[nums[x]] = 1;
+        }
+
+        for (int y = 0; y < checking_values.size(); y++){
+            if (checking_values[y] != 1){
+                return y;
+            }
+        }
+
+
         return 0;
     }
 };
