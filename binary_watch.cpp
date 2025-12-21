dd3d81d26d5abd59e9b223c75a5f948c52577742
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-21 08:50:20 -0800

    first investigation comment

---
+++ binary_watch.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        //so the hours are a nibble 8 4 2 1 (values valid are 0-11)
+        //and the minutes are a nibble and a half 32 16 8 4 2 1 (values valid 0-59)
+
+
+    }
+};
