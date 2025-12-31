02b38ac0a9b538efdc35178990bcd40b18857b75
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-31 11:02:42 -0800

    more wip

--- binary_gap.cpp
+++ binary_gap.cpp
@@ -6,7 +6,14 @@ public:
 
         int mask = 0x01;
 
-        for (int x = 0; x < n)
+        int highest_counter = 0;
+        int curr_counter = 0;
+
+        //first check that it starts at a 1 then keep track of the times it shows up a 1 then 2 different cases based if 
+        //there already was a one or not
+        for (int x = 0; x < 10; x++){
+            
+        }
 
     }
 };
