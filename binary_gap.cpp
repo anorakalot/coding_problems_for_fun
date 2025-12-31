9298817b7398961da14ccaa654ce5ca77d7cc195
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-31 10:43:35 -0800

    first investigation comment

---
+++ binary_gap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        //ok so a preliminary solution is to do just a bit loop over each bit (masking with and to get only the value we're looking at right now)
+        // then left shift that bit mask by 1
+
+        int mask = 0x01;
+
+        for (int x = 0; x < n)
+
+    }
+};
