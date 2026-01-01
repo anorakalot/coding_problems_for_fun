d90b7beeae6f07572b698edbb2f0d8d453f2a849
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-01 12:06:42 -0800

    first investigation comment

---
+++ plus_one.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        //oh so something like get the size then multiply by the power of 10 that's needed
+        //then do O(N) to go through the loop adding each value then get the int value then add one to it
+
+        //or maybe there's a simpler way doing it without converting the array itself.
+    }
+};
