f9b61e679930771afad045e19c8759b8cacc98f5
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2025-12-31 21:14:37 -0800

    137/261 test cases passed

--- binary_gap.cpp
+++ binary_gap.cpp
@@ -43,6 +43,7 @@ public:
                     if (curr_counter > highest_counter){
                         highest_counter = curr_counter;
                     }
+                    curr_counter = 0;
                     first_one_is_found = true;
                 }
             }
