bea179b3e46402ef7f8042f7acb04857a4a03ee7
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-02 11:32:16 -0800

    more wip almost have a soln just edge case left

--- plus_one.cpp
+++ plus_one.cpp
@@ -5,5 +5,28 @@ public:
         //then do O(N) to go through the loop adding each value then get the int value then add one to it
 
         //or maybe there's a simpler way doing it without converting the array itself.
+
+        //because the other way is to do 2 conversions seems like a lot
+
+        //maybe there's a pattern always up the last index up but if it's 9 add 1 to the previous index and so forth
+
+        //start at the last and work way up to the msb
+        int curr_index = digits.size() - 1;
+        while(true){
+            if (digits[curr_index] < 9){
+                digits[curr_index] += 1;
+                break;    
+            }
+            else{
+                curr_index -= 1;
+                //need to add a test case where I add another index to the front if the curr_index is 0
+                if (curr_index == 0){
+                     digits.insert(digits.begin(), 0);
+                }
+            }
+        }
+
+        return digits;
+
     }
 };
