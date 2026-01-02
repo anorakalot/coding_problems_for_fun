ae598c6496cbb63a9f24c77037177b5969fb1271
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-02 15:48:30 -0800

    first investigation comment

---
+++ excel_sheet_column_title.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        //so the thing is divide by 26 and that's how many characters it goes up by
+        //for the first one?
+
+        //is there a way to automate the thing maybe a hashmap like 1 to A 2 to B 
+        //that would save time execution but would take a little time to setup.
+    }
+};
