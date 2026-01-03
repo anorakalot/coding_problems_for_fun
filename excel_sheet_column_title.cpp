e531441e7f217669fe8f8fc484c37ee1a2722230
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-03 08:59:34 -0800

    more investigation

--- excel_sheet_column_title.cpp
+++ excel_sheet_column_title.cpp
@@ -6,5 +6,14 @@ public:
 
         //is there a way to automate the thing maybe a hashmap like 1 to A 2 to B 
         //that would save time execution but would take a little time to setup.
+
+        //then the next thing is to find out how many letters there are
+        //keep going until it's 26? then 
+
+        //but that only seems to work for up to 2 what about 3 or more
+
+        string return_str = "";
+
+        return return_str;
     }
 };
