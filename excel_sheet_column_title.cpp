acbfa16fb310af7d7c7f1a4033ef75e55b0fc757
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-07 15:36:43 -0800

    more wip fixed ZZ edge case more investigation for 3 letters

--- excel_sheet_column_title.cpp
+++ excel_sheet_column_title.cpp
@@ -4,12 +4,11 @@ public:
 
         string return_str;
 
-
         int curr_val = columnNumber;
         char first_char = '@';
         char second_char = '@';
 
-        if (curr_val < 26){
+        if (curr_val <= 26){
             first_char += curr_val;
             curr_val = -1;
             //set it to 0
@@ -17,6 +16,10 @@ public:
 
         while (curr_val >26){
             curr_val -= 26;
+            //maybe something here since 703 instead of AAA is [A  ? oh but [ is right after Z in the ascii chart
+            // if (first_char <= 'A'){
+            //     break;
+            // }
             first_char+=1;
         }
         
@@ -26,14 +29,19 @@ public:
 
         cout <<"curr_val before second char" <<curr_val<< endl;
         //do the secondchar;
-        if (curr_val < 26 && curr_val != -1){
+        if (curr_val <= 26 && curr_val != -1){
             second_char += curr_val;
             curr_val = -1;
             return_str.push_back(second_char);
         }
 
-        
 
+        //ok so the thing is I know the loop structure should be 
+        //curr_val -= 26 until it gets to less than 26
+
+        // need to find out how to make this loop work with multiple letters (not just 2)
+
+        //maybe do it manually for 3 letters and see if that helps find the correct loop algo?
         return return_str;
     }
 };
