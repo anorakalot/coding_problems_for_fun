3ddb4229a3fb15ae67f7ba363df88952a83f03b9
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-20 15:32:00 -0800

    paint file on robot design and more design comments

---
+++ tatami_bot/tatami_robot_maybe.ino
@@ -0,0 +1,78 @@
+//ok so this will be the starting file for the tatami robot i'll speeprate it into seperate files as I work on it
+////////////////////////////////
+//DESIGN
+//can't use stepper motors due to size and weight of tatami
+////have to use dc encoders code can take that from my senior design project in order to go straight but even then I knew that was fully straight
+//
+//
+//need to figure out ways to keep it straight design wise
+//seems to just need to
+// maybe a line follower robot is best because you'd just have to lay doenw the line ahead of time
+// maybe make it like a certain material so it's easy to lay down and works with a lot of different floor colors
+
+//otherwise loading in a route all digitally ups the complexity a lot.
+//And I think it leads to easier debugging since if something's going wrong it's likely due to the floor type messing with the sensors so maybe sensor fusion with a computer vision system?
+
+//rj's response
+//Then the competitors will know what the robot is going to do
+//The idea is that they need to react on the fly to a moving opponent
+//It would definitely make the movement programming easier but take away the fun of a chaotic target
+
+
+//so no line following
+//so maybe combination of imu, and ir sensors, and dc motor encoders (and maybe computer vision?)
+// and maybe have it be inside a cage of some sort?
+// but ir sensors are finicky
+//maybe laser (Lidar)? that seems good
+// the space would just have
+
+// but how would that be effected with the person standing right by it? 
+// have a thing in the code that takes the distance measurement of the other hopefully 3 unobstructed measurements
+
+// need to find a way to also
+// use ros? and simulate robot in gazebo.
+////////////////////////////////
+
+
+/*In this basic free RTOS tutorial will use xTaskCreatePinnedToCore() . 
+in order to enable us to select which ESP32 core (core 0 or core 1) will run the particular task.*/
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED 15
+
+TaskHandle_t LEDBlinking = NULL;  ///  Task handler fot the LED Task
+
+//==============================================================================================//
+// Function to be implemented the LED task is called 
+void BlinkingLED(void *arg){
+  for(;;){
+    Serial.println("LED_Task To Core0");
+    digitalWrite(LED, HIGH);
+    vTaskDelay(250/ portTICK_RATE_MS);
+    digitalWrite(LED, LOW);
+    vTaskDelay(250/ portTICK_RATE_MS);
+  }
+}
+//===========================================================================================//
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  Serial.println("Hello, ESP32 with FreeRTOS !");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+                        // TaskFunction_t pvTaskCode,
+                        //  const char * const pcName,
+                        //  const configSTACK_DEPTH_TYPE uxStackDepth,
+                        //  void *pvParameters,
+                        //  UBaseType_t uxPriority,
+                        //  TaskHandle_t *pxCreatedTask
+                        //last is core which is specific to esp32
+
+  xTaskCreatePinnedToCore(BlinkingLED, "BlinkingLED",3000, NULL, 10, &LEDBlinking, 0);
+}
