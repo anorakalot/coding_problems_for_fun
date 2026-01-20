c3fbc22a0512a248de059934e14193b68c35c8eb
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-20 14:11:57 -0800

    freertos starting code copied from wokwi and modi
    fied by me to just use the builtinLED, starting design comments on how to approach this robot

---
+++ tatami_robot_maybe.ino
@@ -0,0 +1,54 @@
+//ok so this will be the starting file for the tatami robot i'll speeprate it into seperate files as I work on it
+////////////////////////////////
+//DESIGN
+//can't use stepper motors due to size and weight of tatami
+////have to use dc encoders code can take that from my senior design project in order to go straight but even then I knew that was fully straight
+//
+//
+//need to figure out ways to keep it straight design wise
+//seems to just need to
+////////////////////////////////
+
+
+/*In this basic free RTOS tutorial will use xTaskCreatePinnedToCore() . 
+in order to enable us to select which ESP32 core (core 0 or core 1) will run the particular task.*/
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED 15
+
+TaskHandle_t LEDBlinking = NULL;  ///  Task handler fot the LED Task
+
+//==============================================================================================//
+// Function to be implemented the LED task is called 
+void BlinkingLED(void *arg){
+  for(;;){
+    Serial.println("LED_Task To Core0");
+    digitalWrite(LED, HIGH);
+    vTaskDelay(250/ portTICK_RATE_MS);
+    digitalWrite(LED, LOW);
+    vTaskDelay(250/ portTICK_RATE_MS);
+  }
+}
+//===========================================================================================//
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  Serial.println("Hello, ESP32 with FreeRTOS !");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+                        // TaskFunction_t pvTaskCode,
+                        //  const char * const pcName,
+                        //  const configSTACK_DEPTH_TYPE uxStackDepth,
+                        //  void *pvParameters,
+                        //  UBaseType_t uxPriority,
+                        //  TaskHandle_t *pxCreatedTask
+                        //last is core which is specific to esp32
+
+  xTaskCreatePinnedToCore(BlinkingLED, "BlinkingLED",3000, NULL, 10, &LEDBlinking, 0);
+}
