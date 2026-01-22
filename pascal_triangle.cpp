0f07654407b77937a854548e7aafb44c78b2656f
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-21 17:39:53 -0800

    first wip

---
+++ pascal_triangle.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        //it always starta with ones on the first and last indexes
+        // and it increases the number by 1 per each one going down
+    }
+};
