09dded5601602ff5fede79f3d691329d27de2493
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-26 13:09:04 -0800

    found out I mixed up the data types

--- crawler_log_folder.cpp
+++ crawler_log_folder.cpp
@@ -4,29 +4,33 @@ public:
         //ok so read it in until we get to at least a /
         //keep track of the current level?
 
-        int curr_level = 0;
-        string curr_str = "";
-        for(int x = 0; x < logs.size(); x++){
+//oh wait ti's already sepereated
+    for (int x = 0; x < logs.size(); x++){
+        
+    }
+    //     int curr_level = 0;
+    //     string curr_str = "";
+    //     for(int x = 0; x < logs.size(); x++){
             
-            curr_str.push_back(logs[x])
-            if(logs[x] == '/'){
-                if(curr_str == "../"){
-                    if(curr_level > 0){
-                        curr_level -=1;
-                    }
-                }
-                else if (curr_str = "./"){
-                    //nothing
-                }
-                //means it's the move to child folder one
-                else{
-                    curr_level += 1;
-                }
+    //         curr_str.push_back(logs[x]);
+    //         if(logs[x] == '/'){
+    //             if(curr_str == "../"){
+    //                 if(curr_level > 0){
+    //                     curr_level -=1;
+    //                 }
+    //             }
+    //             else if (curr_str = "./"){
+    //                 //nothing
+    //             }
+    //             //means it's the move to child folder one
+    //             else{
+    //                 curr_level += 1;
+    //             }
             
-                //empty curr_str
-                curr_str = "";
-            }//end of logs[x]
-        }
-        return current_level;
-    }//end of function
+    //             //empty curr_str
+    //             curr_str = "";
+    //         }//end of logs[x]
+    //     }
+    //     return current_level;
+    // }//end of function
 };
