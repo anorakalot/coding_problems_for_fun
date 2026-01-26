36b1e206b29eea9ad1783550a1052e99910334ba
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-26 12:07:08 -0800

    first wip

---
+++ crawler_log_folder.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        //ok so read it in until we get to at least a /
+        //keep track of the current level?
+
+        int current_level = 0;
+        string curr_str = "";
+        for(int x = 0; x < logs.size(); x++){
+                
+        }
+    }
+};
