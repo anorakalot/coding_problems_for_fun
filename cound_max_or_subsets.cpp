82eeac33bbb03d3d74dbcb23d220c98e4103cd69
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-27 17:02:07 -0800

    wip investigation

--- cound_max_or_subsets.cpp
+++ cound_max_or_subsets.cpp
@@ -3,7 +3,6 @@ public:
     int countMaxOrSubsets(vector<int>& nums) {
         //trying to think of a bitwise approach that's not just o(n^2) looping through and trying each possible combination
 
-
         //wouldn't the max always be all of them together? then it's just a matter of which subsets equal it?
 
         int max_value = 0;
@@ -13,6 +12,8 @@ public:
 
         cout << max_value << endl;
 
+        //would a good way to be checking if the value has a bit set at one of them the max_value is set at?
+        //get the subset 2^n then minus all the ones that aren't have the bit values?
 
         return 0;//placeholder
     }
