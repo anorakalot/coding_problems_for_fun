f03772923374f7e398406441f92150a0a1f9c115
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-29 17:43:07 -0800

    first wip

---
+++ single_number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //maybe something with and?
+
+        int curr_val = ;
+        for (int x = 0; x < nums.size(); x++){
+
+        }
+
+        
+    }
+};
