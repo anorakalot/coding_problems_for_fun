abeb0d8418e12c8da266fd9d902894368d9aa910
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-01-31 12:48:12 -0800

    first wip

---
+++ assign_cookies.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        //ok so 2 pointers?
+        // is the arrays already ordered?
+
+        //if so
+        //if content move to the next cookie in the index
+    }
+};
