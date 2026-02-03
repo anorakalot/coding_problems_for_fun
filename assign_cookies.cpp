a263cbf511409fe5f93ded7b347b0a04d3ade090
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-02-03 15:23:25 -0800

    15/25 test cases passed

--- assign_cookies.cpp
+++ assign_cookies.cpp
@@ -6,5 +6,29 @@ public:
 
         //if so
         //if content move to the next cookie in the index
+
+        int g_index = 0;
+        int s_index = 0;
+
+        int num_of_hpy_chs =  0;
+
+        int size_of_g = g.size();
+        int size_of_s = s.size();
+
+
+
+        while(s_index < size_of_s && g_index < size_of_g){
+            if(s[s_index] >= g[g_index]){
+                num_of_hpy_chs += 1;
+                s_index += 1;
+                g_index += 1;
+            }
+            else if (s[s_index] < g[g_index]){
+                s_index += 1;
+            }
+        }
+        
+    
+        return num_of_hpy_chs;
     }
 };
