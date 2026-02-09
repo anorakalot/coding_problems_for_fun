811e05f291c67d9e3aa73c1f15e7350707c0ccf4
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-02-09 12:36:49 -0800

    wip found out arrays are not ordered

--- assign_cookies.cpp
+++ assign_cookies.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+        
         //ok so 2 pointers?
         // is the arrays already ordered?
+        //they're not lol
 
         //if so
         //if content move to the next cookie in the index
@@ -15,19 +18,35 @@ public:
         int size_of_g = g.size();
         int size_of_s = s.size();
 
+        //edge case
+        if(size_of_s == 0){
+            return 0;
+        }
+
         //0 is normal 1 is backward
         bool g_dir = 0;
+        bool s_dir = 0;
 
         if(g[0] > g[size_of_g-1]){
             g_index = size_of_g - 1;
             g_dir = 1;
         }
-        
+
+        if(s[0] > s[size_of_s-1]){
+            s_index = size_of_s - 1;
+            s_dir = 1;
+        }
 
         while(s_index < size_of_s && g_index < size_of_g && s_index >=0 && g_index >= 0){
             if(s[s_index] >= g[g_index]){
                 num_of_hpy_chs += 1;
-                s_index += 1;
+                if(s_dir == 1){
+                    s_index -=1;
+                }
+                else{
+                    s_index += 1;
+                }
+
                 if(g_dir == 1){
                     g_index -= 1;
                 }
@@ -39,7 +58,6 @@ public:
                 s_index += 1;
             }
         }
-        
     
         return num_of_hpy_chs;
     }
