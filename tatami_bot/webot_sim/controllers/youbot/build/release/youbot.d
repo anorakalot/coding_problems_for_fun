ca120b6724daaabca30c93fd16c9f15dbd75acab
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-02-10 19:27:35 -0800

    using the youbot proto as a base almost fixed compiling
    for my own version to use for my initial tatami_bot controller coding

---
+++ tatami_bot/webot_sim/controllers/youbot/build/release/youbot.d
@@ -0,0 +1,9 @@
+build/release/youbot.o: youbot.c \
+ /mnt/c/Program\ Files/webots/include/controller/c/webots/keyboard.h \
+ /mnt/c/Program\ Files/webots/include/controller/c/webots/types.h \
+ /mnt/c/Program\ Files/webots/include/controller/c/webots/robot.h \
+ /mnt/c/Program\ Files/webots/include/controller/c/webots/nodes.h \
+ /mnt/c/Program\ Files/webots/projects/robots/kuka/youbot/libraries/youbot_control/include/arm.h \
+ /mnt/c/Program\ Files/webots/projects/robots/kuka/youbot/libraries/youbot_control/include/base.h \
+ /mnt/c/Program\ Files/webots/include/controller/c/webots/types.h \
+ /mnt/c/Program\ Files/webots/projects/robots/kuka/youbot/libraries/youbot_control/include/gripper.h
