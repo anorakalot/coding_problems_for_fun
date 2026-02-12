be345b4abe9f3e901ce0d2c064520419491063b4
Richard "Dylan" Onate McGee <dylanonate@gmail.com>
2026-02-12 13:09:33 -0800

    just psuedocode for interview prep

---
+++ count_and_say.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        //solving it throughn psuedo code since that's what is on the interview
+
+        //ok so iteratively is base up in terms of the 
+
+        //base 1
+        //then afterwards 
+        
+        //ok so the worse solution is to just rego through each array each time remakeing it each time going through the array
+
+        //5 would be 
+        //111221
+
+
+        //so it would be a for loop have the current value set to and what the curr value changes append to the new array
+        //with the ammount counted
+
+        return "";   
+    }
+};
